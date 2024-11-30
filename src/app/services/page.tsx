f50222110da5db6ada9c19d1219247df78231f28
panglimamhmmd <yahyakoto@gmail.com>
2024-11-30 23:51:03 +0700

    fix: removing unused line

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,10 +1,8 @@
 import { Container } from '@/components/Container';
 import { Hero } from '@/components/Hero';
 import { SectionTitle } from '@/components/SectionTitle';
-import { Benefits } from '@/components/Benefits';
 import { Video } from '@/components/Video';
 import { Testimonials } from '@/components/Testimonials';
-import { Faq } from '@/components/Faq';
 import { Cta } from '@/components/Cta';
 
 import { benefitOne, benefitTwo } from '@/components/data';
